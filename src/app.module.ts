060b5f896cfb0333facd76b3b64daf5c4d76cd93
NoistNT <arielgnr23@gmail.com>
2024-01-13 11:09:39 -0300

    Update app controller, module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { TeamModule } from './team/team.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { PlaywrightModule } from './playwright/playwright.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TeamModule, UserModule, AuthModule],
+  imports: [TeamModule, PlaywrightModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getWelcome(): {
+    service: string;
+    version: string;
+    teams: { list: string; team: string };
+  } {
+    return {
+      service: 'info-liga-argentina',
+      version: '1.0.0',
+      teams: {
+        list: 'localhost:3001/team',
+        team: 'localhost:3001/team/1',
+      },
+    };
   }
 }
