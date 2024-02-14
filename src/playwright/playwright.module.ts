80fe69682314af7c16e33c5d8e3974c8bd463270
NoistNT <arielgnr23@gmail.com>
2024-02-14 16:47:46 -0300

    Remove unused files

--- src/playwright/playwright.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PlaywrightController } from './playwright.controller';
-import { PlaywrightService } from './playwright.service';
-
-@Module({
-  controllers: [PlaywrightController],
-  providers: [PlaywrightService],
-})
-export class PlaywrightModule {}
