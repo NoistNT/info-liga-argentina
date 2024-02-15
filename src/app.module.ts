318c6b04c66c3bdcc940321e0dfe3c38eb62b6e8
NoistNT <arielgnr23@gmail.com>
2024-02-15 11:34:59 -0300

    Add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeamModule } from './team/team.module';
-import { PlaywrightModule } from './playwright/playwright.module';
+import { TeamScraperModule } from './team-scraper/team-scraper.module';
+import { SeederModule } from './seeder/seeder.module';
 
 @Module({
-  imports: [TeamModule, PlaywrightModule],
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot(),
+    // Connect to MongoDB using MongooseModule
+    MongooseModule.forRootAsync({
+      // Import ConfigModule to use ConfigService
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        // Get MongoDB URI from ConfigService
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      // Inject ConfigService
+      inject: [ConfigService],
+    }),
+    TeamModule,
+    TeamScraperModule,
+    SeederModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
