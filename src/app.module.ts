37d5355049446aa501d28f3ff33583dba35f19cd
NoistNT <arielgnr23@gmail.com>
2024-02-15 11:42:01 -0300

    Clean code removing comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,12 @@ import { SeederModule } from './seeder/seeder.module';
 
 @Module({
   imports: [
-    // Load environment variables
     ConfigModule.forRoot(),
-    // Connect to MongoDB using MongooseModule
     MongooseModule.forRootAsync({
-      // Import ConfigModule to use ConfigService
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        // Get MongoDB URI from ConfigService
         uri: configService.get<string>('MONGODB_URI'),
       }),
-      // Inject ConfigService
       inject: [ConfigService],
     }),
     TeamModule,
